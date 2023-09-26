3832ba9048c213fbaabd27cdaa63155be1ccb77e
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2023-09-26 10:16:29 -0700

    Printing out logs in the terminal

--- cogs/al_news.py
+++ cogs/al_news.py
@@ -1,6 +1,8 @@
 from asyncio import sleep
 from typing import Any, Coroutine
 from dotenv import dotenv_values as dv
+from datetime import datetime
+import pytz
 
 import discord
 from discord import app_commands as ac
@@ -12,6 +14,7 @@ class Al_news(cmd.Cog):
   def __init__(self, bot: cmd.Bot) -> None:
     self._bot: cmd.Bot = bot
     self._ENV: dict[str, str | None] = dv('.env')
+    self._timezone = pytz.timezone('America/Los_Angeles')
     
     self._twitter: Twitter = Twitter()
     
@@ -30,10 +33,14 @@ class Al_news(cmd.Cog):
   async def on_ready(self):
     await self._bot.wait_until_ready()
       
-  @tasks.loop(hours=1.0)
+  @tasks.loop(minutes=15.0)
   async def al_updates(self):
+    timenow = datetime.now(self._timezone).strftime("%m/%d/%Y %H:%M:%S %Z%z")
     if await self._twitter.start():
       await self.send_tweets(int(self._ENV['CHANNEL']))
+      print(f'[{timenow}]: New tweet(s) sent.')
+    else:
+      print(f'[{timenow}]: Everything is up to date. No tweet was sent.')
     
   @al_updates.before_loop
   async def before_al_updates(self):
@@ -43,7 +50,7 @@ class Al_news(cmd.Cog):
     channel = self._bot.get_channel(channel_id)
     
     for tweet in self._twitter.get_urls():
-      await channel.send(content=tweet)
+      await channel.send(content=f'{self._ENV["ROLE"]}\n\n{tweet}')
       await sleep(5)
       
     
