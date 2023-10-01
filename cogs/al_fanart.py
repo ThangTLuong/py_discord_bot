0a1a83179410c62f58c85436493c85b9fa12e15b
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2023-10-01 02:26:25 -0700

    Fixed albomb command not working sometime

--- cogs/al_fanart.py
+++ cogs/al_fanart.py
@@ -16,75 +16,91 @@ class Al_fanart(cmd.Cog):
     self._time: Time_Log = Time_Log()
     
     self._danbooru: Danbooru = Danbooru()
-    self._list_of_images: deque = deque()
+    self._new_images: deque = deque()
+    self._ready_images: deque = deque()
     self._lock = asyncio.Lock()
     
-  @cmd.Cog.listener()
-  async def on_ready(self):
-    await self._bot.wait_until_ready()
+    self._condition: asyncio.Condition = asyncio.Condition()
     
-    await self._time.now()
-    await self._time.print_time('Initializing AL fanarts...')
+    self.replenishing_fanart.start()
+    
+  @cmd.Cog.listener()
+  async def on_ready(self) -> None:
     
     tasks: list[asyncio.Task] = []
     
-    for _ in range(10):
+    for _ in range(5):
       task = asyncio.create_task(self._danbooru.start())
       tasks.append(task)
       
     results = await asyncio.gather(*tasks)
     
     async with self._lock:
-      # Get lock  
+      # Get lock
       for file_name, image in results:
-        self._list_of_images.append((file_name, image))
+        if len(self._ready_images) >= 100:
+          break
+        
+        self._ready_images.append((file_name, image))
       # Release lock
       
-    await self._time.now()
-    await self._time.print_time('AL fanarts initialization complete.')
+    await self._bot.wait_until_ready()
+    
+  @tasks.loop(seconds=30.0)
+  async def replenishing_fanart(self):
+    await self.parse_images(5)
+    
+  @replenishing_fanart.before_loop
+  async def before_replenishing_fanart(self):
+    await self._bot.wait_until_ready()
     
   @cmd.command(name='alart')
-  async def al_art(self, ctx):
+  async def al_art(self, ctx) -> None:
     await self._time.now()
     await self.send_image(ctx, 1)
     await self._time.print_time('An AL fanart was sent.')
-    
-    async with self._lock:
-      # Get lock
-      file_name, image = await self._danbooru.start()
-      self._list_of_images.append((file_name, image))
-      # Release lock
-      
+
   @cmd.command(name='albomb')
-  async def al_art_bomb(self, ctx):
+  async def al_art_bomb(self, ctx) -> None:
     await self._time.now()
     await self.send_image(ctx, 5)
     await self._time.print_time('An AL bomb of fanart was launched.')
     
-    tasks: list[asyncio.Task] = []
-    for _ in range(5):
-      task = asyncio.create_task(self._danbooru.start())
-      tasks.append(task)
-        
-    results = await asyncio.gather(*tasks)
-    
-    async with self._lock:
-      # Get lock
-      for file_name, image in results:
-        self._list_of_images.append((file_name, image))
-      # Release lock
-    
-  async def send_image(self, ctx, number_of_loops: int):
+  async def send_image(self, ctx, number_of_loops: int) -> None:
     tasks: list[asyncio.Task] = []
     files: list[discord.File] = []
+    
+    # async with self._lock:
+    #   # Get lock
+    #   if len(self._ready_images) < number_of_loops:
+    #     self._ready_images, self._new_images = self._new_images, self._ready_images
+    #   # Release lock
+    
+    # All tasks must stop and check the condition, 
+    #   (len(self._ready_images)-(current task number * number_of_loops)) >= number_of_loops
+    # If the condition returns true for the first task, then it can continue executing
+    # If the condition returns false for the second task, it and the following tasks
+    #   must wait until the first task is done.
+    # Then run this line: self._ready_images, self._new_images = self._new_images, self._ready_images
+    # Then restart the check: (len(self._ready_images)-(current task number * number_of_loops)) >= number_of_loops
+
+    async with self._condition:
+      await self._condition.wait_for(lambda: len(self._ready_images) >= number_of_loops)
     
     async with self._lock:
       # Get lock
       for _ in range(number_of_loops):
-        file_name, image = self._list_of_images.popleft()
+        file_name, image = self._ready_images.popleft()
         task = asyncio.create_task(self.process_image(file_name, image))
         tasks.append(task)
       # Release lock
+      
+    async with self._lock:
+      if len(self._ready_images) < number_of_loops:
+        print(f'There are {len(self._ready_images)} images left in self._ready_images. Switching...')
+        self._ready_images, self._new_images = self._new_images, self._ready_images
+      else:
+        print(f'There are {len(self._ready_images)} images left in self._ready_images.')
       
     results = await asyncio.gather(*tasks)
     
@@ -93,6 +109,37 @@ class Al_fanart(cmd.Cog):
       files.append(file)
       
     await ctx.channel.send(files=files)
+    
+  async def parse_images(self, limit: int = 0) -> None:
+    images_to_get: int
+
+    async with self._lock:
+      images_to_get = min(100 - len(self._new_images), limit if limit else 10)
+    
+    if images_to_get <= 0:
+      return
+    
+    await self._time.now()
+    await self._time.print_time('Resupplying fanarts...')
+    tasks: list[asyncio.Task] = []
+    
+    for _ in range(images_to_get):
+      task = asyncio.create_task(self._danbooru.start())
+      tasks.append(task)
+      
+    results = await asyncio.gather(*tasks)
+    
+    async with self._lock:
+      # Get lock
+      for file_name, image in results:
+        if len(self._new_images) >= 100:
+          break
+        
+        self._new_images.append((file_name, image))
+      # Release lock
+      
+    await self._time.now()
+    await self._time.print_time('Resupplying fanarts complete.')
       
   async def process_image(self, file_name: str, image: bytes) -> tuple[str, BytesIO]:
     return file_name, BytesIO(image)
